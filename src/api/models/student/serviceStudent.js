d68a02826de6668d0cdafe869710b30119a6716b
Eduardo Melo <31355864+eeduardomelo@users.noreply.github.com>
2024-08-27 13:36:52 -0300

    Update serviceStudent.js

--- src/api/models/student/serviceStudent.js
+++ src/api/models/student/serviceStudent.js
@@ -1,5 +1,5 @@
 const studentsModel = require('./studentsModel')
 studentsModel.methods(['get', 'post', 'put', 'delete'])
 studentsModel.updateOptions({new: true, runValidators:true})
-
+console.log(12312);
 module.exports = studentsModel
